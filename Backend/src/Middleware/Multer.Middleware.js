160b41a946e442a20944ca369a10fb92134c8533
Rishu Raj <rishuraj1127@gmail.com>
2024-12-26 17:26:19 +0530

    fixed issue

--- Backend/src/Middleware/Multer.Middleware.js
+++ Backend/src/Middleware/Multer.Middleware.js
@@ -2,6 +2,7 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
