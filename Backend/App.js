2a714269a4136af9aeca2f07dec051a08713addb
Rishu Raj <rishuraj1127@gmail.com>
2025-01-02 18:42:30 +0530

    issue

--- Backend/App.js
+++ Backend/App.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Enabling CORS
 const corsOptions = {
-   origin: 'e-commerce-mu-amber-16.vercel.app', 
+   origin: 'https://e-commerce-mu-amber-16.vercel.app/', 
   //origin: ' http://localhost:5173',
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
